68c7a13792d5a2cc41359af2ac0e420c106563be
Mahmudxon <mahmudxon98@inbox.ru>
2022-06-08 19:06:15 +0500

    Test With Image

--- app/src/main/java/uz/mahmudxon/test/MainActivity.kt
+++ app/src/main/java/uz/mahmudxon/test/MainActivity.kt
@@ -5,36 +5,46 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.animation.RotateAnimation
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var image: ImageView
+    lateinit var constraintLayout: ConstraintLayout
+    var sensorManager: SensorManager? = null
+    var accelerometer: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        image = findViewById(R.id.image)
-        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val sensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
-        sensorManager.registerListener(Screen, sensor, SensorManager.SENSOR_DELAY_UI)
+        constraintLayout = findViewById(R.id.constraintLayout)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_GRAVITY)
         Screen.orientationListener = { current, next ->
             var c = Screen.getDegrees(current)
             var n = Screen.getDegrees(next)
 
-            if(n == 180f)
-            {
-                n = if(c > 0f) 180f else -180f
+            if (n == 180f) {
+                n = if (c > 0f) 180f else -180f
             }
-            if(c == 180f)
-            {
-                c = if(n > 0f) 180f else -180f
+            if (c == 180f) {
+                c = if (n > 0f) 180f else -180f
             }
 
-            val anim = RotateAnimation(c, n, image.width / 2f, image.height / 2f)
+            val anim =
+                RotateAnimation(c, n, constraintLayout.width / 2f, constraintLayout.height / 2f)
             anim.duration = 500
             anim.fillAfter = true
-            image.startAnimation(anim)
+            constraintLayout.startAnimation(anim)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager?.registerListener(Screen, accelerometer, SensorManager.SENSOR_DELAY_UI)
+    }
+
+    override fun onPause() {
+        sensorManager?.unregisterListener(Screen)
+        super.onPause()
     }
 }
