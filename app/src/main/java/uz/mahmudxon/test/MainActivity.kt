13b98050c2bfa8458aa20d759e037dd401fee422
Mahmudxon <mahmudxon98@inbox.ru>
2022-06-09 10:16:26 +0500

    Add android icon to rotate

--- app/src/main/java/uz/mahmudxon/test/MainActivity.kt
+++ app/src/main/java/uz/mahmudxon/test/MainActivity.kt
@@ -5,18 +5,18 @@ import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var constraintLayout: ConstraintLayout
+    lateinit var image: ImageView
     var sensorManager: SensorManager? = null
     var accelerometer: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        constraintLayout = findViewById(R.id.constraintLayout)
+        image = findViewById(R.id.image)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_GRAVITY)
         Screen.orientationListener = { current, next ->
@@ -31,10 +31,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             val anim =
-                RotateAnimation(c, n, constraintLayout.width / 2f, constraintLayout.height / 2f)
+                RotateAnimation(c, n, image.width / 2f, image.height / 2f)
             anim.duration = 500
             anim.fillAfter = true
-            constraintLayout.startAnimation(anim)
+            image.startAnimation(anim)
         }
     }
 
